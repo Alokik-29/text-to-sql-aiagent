a46437670ea61e6275ee51302d8bf0803ee79f54
Alokik Gour <alokikgour29@gmail.com>
2026-07-11 00:47:28 +0530

    minor formatting fix in CORS config

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "https://text-to-sql-aiagent.vercel.app",
+    "http://localhost:5173",
+    "https://text-to-sql-aiagent.vercel.app",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
