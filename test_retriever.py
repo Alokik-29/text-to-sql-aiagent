c4ec111664594ea30f77760227c16f16148b1798
Alokik Gour <alokikgour29@gmail.com>
2026-07-11 00:52:37 +0530

    Delete test_retriever.py

--- test_retriever.py
+++
@@ -1,8 +0,0 @@
-from rag.retriever import SchemaRetriever
-
-retriever = SchemaRetriever()
-results = retriever.retrieve("who are the top selling artists")
-for r in results:
-    print(r["table"])
-    print(r["text"])
-    print("---")
